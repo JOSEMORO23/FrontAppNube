e9a74419da47b17363ee38c42082926dd10ba963
Karla Mendez <karla.mendez.est@tecazuay.edu.ec>
2025-06-11 19:14:02 -0500

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontappsegura';
+  title = 'RapiTec';
+  mostrarHeader = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const ruta = event.urlAfterRedirects;
+        this.mostrarHeader = !(ruta === '/login' || ruta === '/registro');
+      }
+    });
+  }
 }
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiBaseUrl:'http://192.168.18.182:8081/api/usuarios'
-};
